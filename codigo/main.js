164ee311525bab4e6667c17c55e8ba7f532ed8c8
Ana clara <128058331+anaclarabairros@users.noreply.github.com>
2024-11-18 11:47:59 -0300

    Add files via upload

--- codigo/main.js
+++ codigo/main.js
@@ -1,5 +1,18 @@
-const numeroSenha= document.querySelector('.parametro-senha-texto');
-let tamanhoSenha= 18;
-numeroSenha.textContent= tamanhoSenha;
-const botoes= document.querySelector('.parametro-senha-botoes');
-botoes[0].onclic= diminuirTamanho;
+const numeroSenha = document.querySelector('.parametro-senha-texto');
+let tamanhoSenha = 18;
+numeroSenha.textContent = tamanhoSenha;
+const botoes = document.querySelectorAll('.parametro-senha-botao');
+botoes[0].onclick = diminuirTamanho;
+function diminuirTamanho(){
+    if(tamanhoSenha>8){
+    tamanhoSenha=tamanhoSenha-1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+ }
+botoes[1].onclick = aumentarTamanho;
+function aumentarTamanho(){
+    if(tamanhoSenha<18){
+    tamanhoSenha=tamanhoSenha+1;
+    numeroSenha.textContent = tamanhoSenha;
+}
+ }
