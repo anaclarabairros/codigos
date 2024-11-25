484a5d9854023ac9b5c5b21c29077d1d88cfe388
Ana clara <128058331+anaclarabairros@users.noreply.github.com>
2024-11-25 11:43:21 -0300

    Add files via upload

--- codigo/main.js
+++ codigo/main.js
@@ -2,17 +2,31 @@ const numeroSenha = document.querySelector('.parametro-senha-texto');
 let tamanhoSenha = 18;
 numeroSenha.textContent = tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha-botao');
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas ="ABCDEFGHIJKLMNOPQRSTUVWXYZ";
 botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentarTamanho;
+geraSenha();
 function diminuirTamanho(){
     if(tamanhoSenha>8){
     tamanhoSenha=tamanhoSenha-1;
     numeroSenha.textContent = tamanhoSenha;
 }
  }
-botoes[1].onclick = aumentarTamanho;
 function aumentarTamanho(){
     if(tamanhoSenha<18){
     tamanhoSenha=tamanhoSenha+1;
     numeroSenha.textContent = tamanhoSenha;
 }
  }
+
+function geraSenha(){
+    let senha = " ";
+    for(let i=0; i<tamanhoSenha; i++){
+    let numeroAleatorio = Math.random()* letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha = senha + letrasMaiusculas[numeroAleatorio];
+}
+    campoSenha.value=senha;
+ }
+
